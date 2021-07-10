315a7efc116a5978743d3d547bbe629629a8a195
gorj3 <jorgecarrilloit@gmail.com>
2021-07-10 05:04:00 +0000

    es cluster completed

--- modules/elasticsearch/main.tf
+++ modules/elasticsearch/main.tf
@@ -27,6 +27,25 @@ resource "azurerm_lb" "elastic_lb" {
   }
 }
 
+resource "azurerm_lb_probe" "elastic_probe" {
+  resource_group_name = var.rg
+  loadbalancer_id     = azurerm_lb.elastic_lb.id
+  name                = "elastic_probe"
+  port                = 9200
+}
+
+resource "azurerm_lb_rule" "elasticrule" {
+  resource_group_name            = var.rg
+  loadbalancer_id                = azurerm_lb.elastic_lb.id
+  name                           = "elasticrule"
+  protocol                       = "Tcp"
+  frontend_port                  = 9200
+  backend_port                   = 9200
+  frontend_ip_configuration_name = "esipconfig"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.es_nodes.id
+  probe_id = azurerm_lb_probe.elastic_probe.id
+}
+
 resource "azurerm_lb_backend_address_pool" "es_nodes" {
   name            = "esnodes"
   loadbalancer_id = azurerm_lb.elastic_lb.id
@@ -91,3 +110,39 @@ resource "azurerm_virtual_machine" "main" {
     environment = "staging"
   }
 }
+
+resource "azurerm_network_security_group" "elastic_nsg" {
+  name                = "elastic_nsg"
+  location            = var.location
+  resource_group_name = var.rg
+
+  security_rule {
+    name                       = "elasticIN"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "9200"
+    destination_port_range     = "9200"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "22"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.0.1.0/24"
+    destination_address_prefix = "*"
+  }
+}
+
+#resource "azurerm_network_interface_security_group_association" "elastic_nsg_as" {
+#  for_each              = toset(var.vm_names)
+#  network_interface_id      = each.value
+#  network_security_group_id = azurerm_network_security_group.example.id
+#}
